01c08d18956c9cc60fad4094f7159a604e42ecb7
boiltaimn <34036969+finsterniz@users.noreply.github.com>
2024-07-17 01:41:37 +0200

    Initial Commit

---
+++ GCDWebUploader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GCDWebUploader
+//
+//  Created by a on 17.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GCDWebUploader/GCDWebUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCDWebUploaderApp.swift
+//  GCDWebUploader
+//
+//  Created by a on 17.07.24.
+//
+
+import SwiftUI
+
+@main
+struct GCDWebUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
